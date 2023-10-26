470f6605256edd5c123f8a083cf8ae95aff7c814
mtulepkaliev <moses1.tule@gmail.com>
2023-10-26 13:52:53 -0600

    Tak Model Created

--- task_tracker/admin.py
+++ task_tracker/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from task_tracker.models import *
 # Register your models here.
+admin.site.register(Task)
--- task_tracker/models.py
+++ task_tracker/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+
+    #TODO remove this as we will be implementing department model later
+    department_choices = [("All","ALL"),("EE","Electrical"), ("MECH","Mechanical"),("BUS","Business")]
+
+    summary = models.CharField(max_length=200)
+    detail = models.TextField(max_length=1000)
+    is_complete = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+
+    #TODO change to FK
+    department = models.CharField(max_length=200,choices=department_choices,default="All",blank=False)
+
+    def __str__(self):
+        return self.summary
