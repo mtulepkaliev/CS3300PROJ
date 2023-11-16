1689651a0667785402a240a9ab33e2a09ff5a114
mtulepkaliev <moses1.tule@gmail.com>
2023-11-16 11:25:16 -0700

    Added Department specific tag filters to navbar

---
+++ task_tracker/templatetags/tags.py
@@ -0,0 +1,13 @@
+#https://stackoverflow.com/questions/21062560/django-variable-in-base-html
+
+from django import template
+from task_tracker.models import Department
+
+register = template.Library()
+
+@register.simple_tag
+def department_name_id_dict(request):
+    mydict = Department.objects.all().values('name','id')
+    print(mydict)
+    print(mydict[0]['id'])
+    return mydict
